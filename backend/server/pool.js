9c814f1f2f2c9d955024e9776f44c346864900ee
aman choudhary <amanchoudharygeo@gmail.com>
2021-01-05 17:52:43 +0530

    changes in pool.js

--- backend/server/pool.js
+++ backend/server/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
+//setup for elephantsql
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
